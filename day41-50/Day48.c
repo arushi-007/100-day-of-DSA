35cb333971062a1f3ddcd43ce164def598337a1b
Arushi-007 <Arushi.22403@stu.upes.ac.in>
2026-03-22 18:45:07 +0530

    code

---
+++ day41-50/Day48.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int countLeaves(struct Node* root) {
+    if (root == NULL) return 0;
+    if (root->left == NULL && root->right == NULL) return 1;
+    return countLeaves(root->left) + countLeaves(root->right);
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+
+    printf("Number of leaf nodes: %d\n", countLeaves(root));
+    return 0;
+}
