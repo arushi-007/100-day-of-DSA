b0cf93c210ac14adfa439448d8ff76c2c866536e
Arushi-007 <Arushi.22403@stu.upes.ac.in>
2026-04-02 11:00:24 +0530

    code

---
+++ day51-60/Day60.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    for (int i = 0; i < n; i++) {
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        if (left < n && arr[i] > arr[left]) {
+            printf("NO\n");
+            return 0;
+        }
+        if (right < n && arr[i] > arr[right]) {
+            printf("NO\n");
+            return 0;
+        }
+    }
+    printf("YES\n");
+    return 0;
+}
